5a3052240258e2bd4fda39faaa62c1ef28270b78
Marko Zecevic <marczecevic@gmail.com>
2021-05-05 10:37:19 +0200

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def full_title(title_text='')
+        basic_title = 'ruby on rails tutorial'
+
+        if title_text.empty?
+            basic_title
+        else
+            "#{title_text} | #{basic_title}"
+        end
+    end
 end
